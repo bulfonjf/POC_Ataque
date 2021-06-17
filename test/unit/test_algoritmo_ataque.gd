3b260f667497e8db5d31dcf07561c8c1b212d05d
Juan Bulfon <bulfonjf@gmail.com>
2021-06-16 21:33:37 -0300

    Add unidad_esperada en tests de algoritmo_ataque

--- test/unit/test_algoritmo_ataque.gd
+++ test/unit/test_algoritmo_ataque.gd
@@ -14,7 +14,7 @@ func test_obtner_unidad_modificada_debe_retornar_la_unidad_con_los_modificadores
 				Buff.new(Valor.new(20), get_atributo_vida(100))
 			],
 			unidad =  UnidadAlgoritmoAtaque.new([get_atributo_vida(100)]),
-			unidad_esperada = UnidadAlgoritmoAtaque.new([get_atributo_vida(120)])
+			unidad_esperada = UnidadAlgoritmoAtaque.new([get_atributo_vida(120)]),
 		},
 		{
 			test_nombre = "unidad con vida 100 - aplicar debuff a la vida de 20 - debe retornar unidad con vida 80",
@@ -22,7 +22,7 @@ func test_obtner_unidad_modificada_debe_retornar_la_unidad_con_los_modificadores
 				Debuff.new(Valor.new(20), get_atributo_vida(100))
 			],
 			unidad =  UnidadAlgoritmoAtaque.new([get_atributo_vida(100)]),
-			unidad_esperada = UnidadAlgoritmoAtaque.new([get_atributo_vida(80)])
+			unidad_esperada = UnidadAlgoritmoAtaque.new([get_atributo_vida(80)]),
 		},
 		{
 			test_nombre = "unidad con vida 100 - aplicar buff de 7 - debe retornar unidad con vida 93",
@@ -59,11 +59,11 @@ func test_obtner_unidad_modificada_debe_retornar_la_unidad_con_los_modificadores
 		unidad_obtenida = null
 		sut.queue_free()
 
-func test_obtner_unidad_modificada_deben_retornar_error():
+#func test_obtner_unidad_modificada_deben_retornar_error():
 	#var atributo_vida_100 = Atributo.new("vida", Valor.new(100), 120, 0)
 	#var buff_vida_20 = Buff.new(Valor.new(20), atributo_vida_100)
 
-	var test_suite = [
+	# var test_suite = [
 		# {
 		# 	test_nombre = "unidad esperada no debe coincidir con unidad obtenida",
 		# 	modificadores =  [
@@ -72,24 +72,24 @@ func test_obtner_unidad_modificada_deben_retornar_error():
 		# 	unidad =  UnidadAlgoritmoAtaque.new([atributo_vida_100]),
 		# 	unidad_esperada = UnidadAlgoritmoAtaque.new([atributo_vida_100])
 		# },
-	]
+	# ]
 
-	for tc in test_suite:
-		# Arrange
-		sut = load("res://combate/algoritmo.gd").new()
+	# for tc in test_suite:
+	# 	# Arrange
+	# 	sut = load("res://combate/algoritmo.gd").new()
 
-		# Act
-		var unidad_obtenida = sut.obtener_unidad_con_modificadores_aplicados(tc.modificadores, tc.unidad)
+	# 	# Act
+	# 	var unidad_obtenida = sut.obtener_unidad_con_modificadores_aplicados(tc.modificadores, tc.unidad)
 
-		# Assert
-		print(tc.test_nombre)
-		print("unidad esperada ", tc.unidad_esperada)
-		print("unidad obtenida ", unidad_obtenida)
-		assert_false(tc.unidad_esperada._eq(unidad_obtenida)) # TODO darle una vuelta de rosca a este assert
+	# 	# Assert
+	# 	print(tc.test_nombre)
+	# 	print("unidad esperada ", tc.unidad_esperada)
+	# 	print("unidad obtenida ", unidad_obtenida)
+	# 	assert_false(tc.unidad_esperada._eq(unidad_obtenida)) # TODO darle una vuelta de rosca a este assert
 		
-		# Teardown
-		unidad_obtenida = null
-		sut.queue_free()
+	# 	# Teardown
+	# 	unidad_obtenida = null
+	# 	sut.queue_free()
 		
 func get_atributo_vida(cantidad : int) -> Atributo:
 	return Atributo.new("vida", Valor.new(cantidad), 200, 0)
